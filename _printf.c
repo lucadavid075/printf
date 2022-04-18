f30212cfdc28857872a2f4efadb6d0a22d19106a
Luke David <daiveedlucas049@gmail.com>
2022-04-18 20:13:15 +0000

    Added the _printf function file for c, s and % format

---
+++ _printf.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _printf - function that replicates what printf does
+ * @format: a character string
+ *
+ * Return:  the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	print_type argument[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"%", _print_percent},
+		
+		{NULL, NULL}
+	};
+	va_list ap;
+	int count = 0;
+
+	va_start(ap, format);
+	count = get_print(format, argument, ap);
+	va_end(ap);
+	return (count);
+}
