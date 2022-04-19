e90b5b52bea4bbe7a274256cb5f0ca54156a83c0
Luke David <daiveedlucas049@gmail.com>
2022-04-19 08:10:37 +0000

    [Fix]

--- _printf.c
+++ _printf.c
@@ -1,27 +1,24 @@
-#include "main.h"                                                        
-                                                                         
-/**                                                                      
- * _printf - function that replicates what printf does                   
- * @format: a character string                                           
- *                                                                       
- * Return:  the number of characters printed                             
- */                                                                      
-int _printf(const char *format, ...)                                     
-{                                                                        
-        print_type argument[] = {                                        
-                {"c", _print_char},                                      
-                {"s", _print_string},                                    
-                {"%", _print_percent},                                   
-                {"d", _print_int},                                       
-                {"i", _print_int},
-                {"b", _print_binary},                                       
-                {NULL, NULL}                                             
-        };                                                               
-        va_list ap;                                                      
-        int count = 0;                                                   
-                                                                         
-        va_start(ap, format);                                            
-        count = get_print(format, argument, ap);                         
-        va_end(ap);                                                      
-        return (count);                                                  
+#include "main.h"
+
+/**
+ * _printf - function that replicates what printf does
+ * @format: a character string
+ *
+ * Return:  the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	print_type argument[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"%", _print_percent},
+		{NULL, NULL}
+	};
+	va_list ap;
+	int count = 0;
+
+	va_start(ap, format);
+	count = get_print(format, argument, ap);
+	va_end(ap);
+	return (count);
 }
