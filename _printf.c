8771144170bfa691dca55517673b45d25c64c709
joyodeyemi <joybella4eva@gmail.com>
2022-04-19 14:40:36 -0700

    fixed betty error in printf file

--- _printf.c
+++ _printf.c
@@ -19,7 +19,8 @@ int _printf(const char *format, ...)
 		{"o", _print_octal},
 		{"x", _print_hex_l},
 		{"X", _print_hex_u},
-		{"p", _print_address},{"R", _print_rot13},
+		{"p", _print_address},
+		{"R", _print_rot13},
 		{NULL, NULL}
 
 	};
