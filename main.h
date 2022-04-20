581cf1d4494ac34a8a0954f3fb9d734b50a4a3a1
Luke David <daiveedlucas049@gmail.com>
2022-04-20 04:21:31 +0000

    [update] Refactored and Rearrange folder structures

--- main.h
+++ main.h
@@ -1,74 +1,76 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <stdlib.h>
 #include <stdarg.h>
-#include <assert.h>
-#include <stdbool.h>
-
-
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-	int zero;
-	int minus;
-} flags_t;
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
+ * struct Checkstr - Struct to check format
+ *
+ * @flag: character associated to flags
+ * @flagnot: Flags that are not allowed per specifier
  */
-typedef struct printHandler
+typedef struct Checkstr
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
+	char flag;
+	char *flagnot;
+} checkstr;
 
-/* _printf prototype */
+/**
+ * struct Specs_t - struct to call specifier functions
+ *
+ * @s: character representing specifier
+ * @f: The function associated
+ */
+typedef struct Specs_t
+{
+	char s;
+	char *(*f)(const char *, int, va_list, int *);
+} specs_t;
+/* Custom printf function */
 int _printf(const char *format, ...);
 
-/* write out functions */
-int _putchar(char c);
-int _puts(char *str);
+/* Auxiliar functions */
+int _strlen(char *s);
+int blen(unsigned long int n, unsigned long int base);
+int blen10(long int n, long int base);
+void rev_str(char *s);
+char *hexS(int n);
+char *rot13(char *s);
+int check_format(const char *formspec, int j);
+int check_specs(const char *s, int *p);
+void _memcpy(char *buffer, const char *src, int *i_b, int *stock, int n);
+void print_number_str(long int n, char *str);
+void print_number_str_u(unsigned long int n, char *str);
+int free_buffer(char *, int);
+int effective_length(char *buffer);
 
-int count_digit(int i);
-void print_number(int n);
-int print_bigS(va_list l, flags_t *f);
-
-/*prototype for all format specifiers */
-int _print_char(va_list l, flags_t *f);
-int _print_string(va_list l, flags_t *f);
-int _print_percent(va_list l, flags_t *f);
-int _print_int(va_list l, flags_t *f);
-int _print_reverse(va_list l, flags_t *f);
-int _print_rot13(va_list l, flags_t *f);
-int _print_unsigned(va_list l, flags_t *f);
-int _print_octal(va_list l, flags_t *f);
-int _print_binary(va_list l, flags_t *f);
-int _print_hex_l(va_list l, flags_t *f);
-int _print_hex_u(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-
-int _print_address(va_list l, flags_t *f);
-
-/* converter prototypes */
-char *convert(unsigned long int num, int base, int lowercase);
-
-/* get_print */
-int (*get_print(char s))(va_list, flags_t *);
-
-/* get_flag */
-int get_flag(char s, flags_t *f);
-
-#endif /* _MAIN_H_ */
+/* Generate strings functions */
+char *generate_malloc(const char *s, int len_p, va_list list, int *);
+char *gen_c(const char *, int, va_list, int *);
+char *gen_s(const char *, int, va_list, int *);
+char *gen_d(const char *, int, va_list, int *);
+char *gen_i(const char *, int, va_list, int *);
+char *gen_o(const char *, int, va_list, int *);
+char *gen_b(const char *, int, va_list, int *);
+char *gen_x(const char *, int, va_list, int *);
+char *gen_X(const char *, int, va_list, int *);
+char *gen_p(const char *, int, va_list, int *);
+char *gen_u(const char *, int, va_list, int *);
+char *gen_S(const char *, int, va_list, int *);
+char *gen_r(const char *, int, va_list, int *);
+char *gen_R(const char *, int, va_list, int *);
+char *gen_per(const char *, int, va_list, int *);
+/* Flags functions */
+unsigned int get_width(const char *pattern, int len_p, va_list list);
+unsigned int get_precision(const char *pattern, int len_p, va_list list);
+long int get_param(const char *pattern, int len_p, va_list list);
+unsigned long int get_param_u(const char *pattern, int len_p, va_list list);
+void app_flags(const char *, int, char *, int *, int, int);
+int app_precision(const char *pattern, int len_p, char *buffer, int pr);
+int app_width(char *buffer, int len_b, int wi);
+int app_hash(const char *pattern, int len_p, char *buffer);
+int app_plus_space(const char *pattern, int len_p, char *buffer);
+int check_flag(const char *pattern, int len_p, char flag);
+#endif
